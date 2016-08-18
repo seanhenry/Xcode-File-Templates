0ba1a4c26e5a4245095373d225a5747c1cab77fc
Sean Henry <hello@seanhenry.codes>
2016-08-18 20:01:09 +0100

    Changes mock template

--- Swift/Swift Mock Class.xctemplate/Mock___VARIABLE_classToMock:identifier___.swift
+++ Swift/Swift Mock Class.xctemplate/Mock___VARIABLE_classToMock:identifier___.swift
@@ -7,12 +7,7 @@
 import XCTest
 @testable import ___VARIABLE_moduleName:identifier___
 
-class Stub___VARIABLE_classToMock:identifier___: ___VARIABLE_classToMock:identifier___ {
-    
-    
-}
-
-class Mock___VARIABLE_classToMock:identifier___: Stub___VARIABLE_classToMock:identifier___ {
+class Mock___VARIABLE_classToMock:identifier___: ___VARIABLE_classToMock:identifier___ {
     
     
 }
