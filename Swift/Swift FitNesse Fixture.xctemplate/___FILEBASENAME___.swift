735516fe8e518f9ff4e37fb29f9f360a7c09de6b
Sean Henry <hello@seanhenry.codes>
2016-08-18 20:00:59 +0100

    Adds FitNesse template

---
+++ Swift/Swift FitNesse Fixture.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,27 @@
+//
+//  ___FILENAME___
+//
+//___COPYRIGHT___
+//
+
+import Foundation
+
+@objc(___FILEBASENAMEASIDENTIFIER___)
+class ___FILEBASENAMEASIDENTIFIER___: NSObject, SlimDecisionTable {
+
+    // MARK: - inputs
+    var <#input#>: String!
+
+    // MARK: - outputs
+    var <#output#>: String!
+
+    // MARK: - test
+
+    func reset() {
+
+    }
+
+    func execute() {
+
+    }
+}
